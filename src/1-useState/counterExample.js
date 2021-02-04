e33c0f1688cb57d92741d4b217622824c93cb3b3
Vertika Jain <vertikajain96@gmail.com>
2021-02-04 22:00:52 +0530

    Complex counter example added

--- src/1-useState/counterExample.js
+++ src/1-useState/counterExample.js
@@ -9,14 +9,27 @@ const CounterExample = () => {
     const subtractCount = () => {
         setCount(count - 1)
     }
-    
+
+    // Complex counter example using setTimeOut
+    const complexCounter = () => {
+        setTimeout(() => {
+            // setCount(count + 1) // updates from the old value of count
+            setCount((prevCount) => { // updates from the previously/recently updated value
+                return prevCount + 1
+            })
+        }, 2000)
+    }
+
     return (
         <>
             <h2>Simple Counter Example</h2>
             <h1>{count}</h1>
             <button className="btn" onClick={addCount}>ADD</button>
-            <button className="btn" onClick={()=> setCount(0)}>RESET</button> {/* Inline function example */}
+            <button className="btn" onClick={() => setCount(0)}>RESET</button> {/* Inline function example */}
             <button className="btn" onClick={subtractCount}>SUBTRACT</button>
+            <h2>Complex Counter Example</h2>
+            <h1>{count}</h1>
+            <button className="btn" onClick={complexCounter}>ADD LATER</button>
         </>
     )
 }
