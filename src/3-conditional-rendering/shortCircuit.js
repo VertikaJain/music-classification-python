48cd4076980910171d089877ddc635a611abe383
Vertika Jain <vertikajain96@gmail.com>
2021-02-05 22:32:53 +0530

    Ternary Operator in React with useState

--- src/3-conditional-rendering/shortCircuit.js
+++ src/3-conditional-rendering/shortCircuit.js
@@ -2,12 +2,17 @@ import React, { useState } from 'react'
 
 const ShortCircuit = () => {
     const [text, setText] = useState('') // '' is falsy in JS
+    const [isError, setIsError] = useState(false)
+
     return (
         <>
             {/* {if(){console.log()}}  => You cannot put conditional while returning in React. */}
             <h1>{text || "vertika Jain"}</h1>
-            {text && <h1>hello world</h1>}
-            {!text && <h1>hello world</h1>}
+            <button className="btn" onClick={() => setIsError(!isError)}>Toggle Error</button>
+            {isError && <h1>Error</h1>}
+
+            {/* Ternary operator example */}
+            {isError ? <p>there is an error</p> : <h2>there is no error</h2>}
         </>
     )
 }
