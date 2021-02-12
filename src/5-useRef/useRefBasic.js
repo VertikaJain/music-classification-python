2bb18e7317f1b292a1f3513d0f1af6b8c1edecb7
Vertika Jain <vertikajain96@gmail.com>
2021-02-12 14:23:00 +0530

    useRef basic example with focus()

--- src/5-useRef/useRefBasic.js
+++ src/5-useRef/useRefBasic.js
@@ -1,15 +1,16 @@
 import React, { useRef, useEffect } from 'react'
 
 // useRef does not trigger re-render
+// UseCase: target DOM elements when required
 
 const UseRefBasic = () => {
     const refContainer = useRef(null)
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(e.target.value);
-        console.log(refContainer.current.value);
-
+        console.log(refContainer.current); // prints the DOM element
+        console.log(refContainer.current.value); // prints the DOM element's value that the user has entered.
+        refContainer.current.focus() // helps to focus/target on the specified DOM element
     }
 
     return (
@@ -17,7 +18,7 @@ const UseRefBasic = () => {
             <form className="form" onSubmit={handleSubmit}>
                 <div>
                     <input type="text" ref={refContainer} />
-                    <button type="submit">Submit</button>
+                    <button type="submit" >Focus</button>
                 </div>
             </form>
         </>
